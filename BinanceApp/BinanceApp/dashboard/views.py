1b3754061503e0e374d9b84e242560d195d44ddc
steed924 <steed924@outlook.com>
2021-11-05 03:19:35 +0900

    Binance API total

--- BinanceApp/BinanceApp/dashboard/views.py
+++ BinanceApp/BinanceApp/dashboard/views.py
@@ -2,10 +2,11 @@ from sqlite3.dbapi2 import Timestamp
 from django.http.response import Http404, HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from datetime import datetime
 from .models import Log
 # Create your views here.
-def compare_volume(e):
-  return e["volume"]
+def compare(e):
+  return e["percent"]
 binance_symbols = ["BTCUSDT", "SHIBUSDT", "ETHUSDT", "BUSDUSDT", "SANDUSDT", "SOLUSDT", "IOTXUSDT", "DOTUSDT",
 "XRPUSDT", "BNBUSDT", "MANAUSDT", "ARPAUSDT", "ADAUSDT", "TRXUSDT", "AVAXUSDT", "AXSUSDT", "DOGEUSDT", "OMGUSDT", "LRCUSDT", "HOTUSDT",
 "LUNAUSDT", "SLPUSDT", "FIDAUSDT", "MATICUSDT", "FTMUSDT", "FILUSDT", "ARUSDT", "BADGERUSDT", "USDCUSDT", "ATOMUSDT", "VETUSDT",
@@ -13,6 +14,7 @@ binance_symbols = ["BTCUSDT", "SHIBUSDT", "ETHUSDT", "BUSDUSDT", "SANDUSDT", "SO
 "TLMUSDT", "ALGOUSDT", "ENJUSDT"]
 def index(request):
     try:
+        now = datetime.now()
         coins_1m = []
         coins_5m = []
         coins_15m = []
@@ -30,16 +32,17 @@ def index(request):
             log = Log.objects.filter(symbol=symbol, interval="1h").order_by('-timestamp')[:2]
             if log.count() > 1:
                 coins_1h.append({"symbol": symbol[0:-4], "volume": (round(log[0].volume, 2)), "percent": (round((log[0].volume - log[1].volume) * 100 / log[1].volume, 2))})
-        coins_1m.sort(reverse=True, key=compare_volume)
-        coins_5m.sort(reverse=True, key=compare_volume)
-        coins_15m.sort(reverse=True, key=compare_volume)
-        coins_1h.sort(reverse=True, key=compare_volume)
+        coins_1m.sort(reverse=True, key=compare)
+        coins_5m.sort(reverse=True, key=compare)
+        coins_15m.sort(reverse=True, key=compare)
+        coins_1h.sort(reverse=True, key=compare)
         template = loader.get_template('index.html')
         context = {
             'coins_1m': coins_1m[0:20],
             'coins_5m': coins_5m[0:20],
             'coins_15m': coins_15m[0:20],
             'coins_1h': coins_1h[0:20],
+            'date': now.strftime("%m/%d/%Y")
         }
     except Log.DoesNotExist:
         raise Http404("Sorry, try again when it finishes loading data...")
